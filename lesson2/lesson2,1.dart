3384b4005d34d2e56a157a227dc1c2dd1fd1cd75
hridoykhan51 <hridoykhan.cse.cu@gmail.com>
2024-01-11 12:53:59 +0600

    touch and see code

--- lesson2/lesson2,1.dart
+++ lesson2/lesson2,1.dart
@@ -1,4 +1,26 @@
+import 'dart:mirrors';
+
 void main() {
   String name = "this is our community";
+  int num = 1;
+
   print(name);
+  print(num);
+
+  String first = "hridoy";
+  String second = "khan";
+  print(first + " " + second);
+
+  //adding, subtraction, divide
+  int a = 50;
+  int b = 50;
+  int c = 50;
+  int d = a + b + c;
+  int sub = a - b + c;
+
+  print(d);
+  print(sub);
+  print(a / b);
+  print(b * c);
+
 }
