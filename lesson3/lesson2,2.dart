2807fc41a6b5d219c100ee9540d02c8f59c4f15a
hridoykhan51 <hridoykhan.cse.cu@gmail.com>
2024-01-11 13:25:16 +0600

    var dynamic and final keyword

---
+++ lesson3/lesson2,2.dart
@@ -0,0 +1,15 @@
+import 'dart:mirrors';
+
+void main() {
+  var name = "HRIDOY KHAN";
+  dynamic name2 = "hridoy khan";
+  name2 = "hridoy khan convert hriody";
+  
+  //this is final variable
+  final name3 = "this is final variable";
+ 
+
+  print(name);
+  print(name2);
+  print(name3);
+}
