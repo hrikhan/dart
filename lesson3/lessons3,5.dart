6faa905a12a085406b011b91954a38efac7448a5
hridoykhan51 <hridoykhan.cse.cu@gmail.com>
2024-01-12 22:09:25 +0600

    map constractor

---
+++ lesson3/lessons3,5.dart
@@ -0,0 +1,12 @@
+void main() {
+  var a = new Map();
+  a['name'] = 'hridoy';
+  a['age '] = '21';
+  a['size'] = '5 feet 7';
+  a['contry'] = 'bangladesh';
+  a['religion'] = 'islam';
+  a['type'] = 'no type';
+  a['nationality'] = 'bangladeshi';
+
+  print(a);
+}
