5806b68eb94d9122d590e1fe0036e5a7acffe09e
hridoykhan51 <hridoykhan.cse.cu@gmail.com>
2024-01-15 19:49:42 +0600

    constractor

---
+++ lesson5/lesson5.1.dart/contractor.dart
@@ -0,0 +1,12 @@
+class contractor {
+  var name;
+  var age;
+
+  contractor(var name, var age) {
+    this.name = name;
+    this.age = age;
+    print('my name is $name');
+    print("the age is $age");
+    print("this is contractor method");
+  }
+}
---
+++ lesson5/lesson5.1.dart/myclass.dart
@@ -0,0 +1,6 @@
+import 'contractor.dart';
+
+void main() {
+  var a = new contractor("hridoy", 20);
+  print(a.name);
+}
